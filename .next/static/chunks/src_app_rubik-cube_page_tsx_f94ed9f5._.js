8ad4d9c5d58f29d289756d37e2f6d4b10d8304b7
WhiteStorm11 <93656141+WhiteStorm11@users.noreply.github.com>
2025-07-02 18:45:58 -0500

    Add .next/static/chunks/src_app_rubik-cube_page_tsx_f94ed9f5._.js 1751499957871

---
+++ .next/static/chunks/src_app_rubik-cube_page_tsx_f94ed9f5._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_three_build_three_core_df0bde7e.js",
+  "static/chunks/node_modules_three_build_three_module_230a9f89.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_2d097f20._.js",
+  "static/chunks/src_a2ba79b8._.js"
+],
+    source: "dynamic"
+});
